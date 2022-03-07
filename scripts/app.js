58871ef9bc2bfecd87a0b63ecda3ecee036be464
shifat514 <shifat514@gmail.com>
2022-03-08 01:12:04 +0600

    name function added

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,24 @@
+
+ let editedPlayer =0;
+
+ const players =[          // array of objects
+    {
+        name:'',
+        symbol:'X',
+    },
+     {
+        name: '',
+        symbol:'O',
+     },
+ ];
+
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
+const errorsOutputElement=document.getElementById('config-errors');
 
+
+//button
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
@@ -12,5 +29,4 @@ editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
-formElement.addEventListener('submit', savePlayerConfig);
-
+formElement.addEventListener('submit',savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,16 +1,55 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+
+ 
+
+  editedPlayer= +event.target.dataset.playerid; //playerid property +'1' as a string covnerts to number 
+ 
   playerConfigOverlayElement.style.display = 'block';
   backdropElement.style.display = 'block';
 }
 
-  function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';
+function closePlayerConfig(){
+  playerConfigOverlayElement.style.display = 'none';
+  backdropElement.style.display = 'none';
+
+  //removing errors
+  formElement.firstElementChild.classList.remove('error'); // to remove the red errors
+  errorsOutputElement.textContent='';
+}
+  
+
+function savePlayerConfig(event){
+  event.preventDefault(); // prevents the browser from sending a request automatially
+  
+  const formData= new FormData(event.target); //blueprint functions
+  const enteredPlayerName= formData.get('playername').trim(); //allows us to get a value from our inputs
+  // trim will get rid of extra white space
+
+  if(!enteredPlayerName) //empty string will be considered a false value
+  {
+      event.target.firstElementChild.classList.add('error'); //dom traversal
+
+      errorsOutputElement.textContent="Enter a valid name!";
+      return;
   }
   
-  function savePlayerConfig(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername');
-    console.log(enteredPlayername);
-  }
+  const updatedPlayerDataElement= document.getElementById('player-'+editedPlayer+'-data');
+  
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+
+  // if(editedPlayer === 1)
+  // {
+  //   players[0].name= enteredPlayerName;
+  // }
+  // else
+  // {
+  //   players[1].name= enteredPlayerName;
+  // }
+
+    players[editedPlayer-1].name=enteredPlayerName;
+
+    closePlayerConfig(); // to close the overlay
+
+}
+ 
